8f50f3887a979f41d2dd23336023a35a919a044b
billytrend <billy.trend@gmail.com>
2015-12-29 16:15:22 +0000

    Implemented basic cli for the BNF parser.

---
+++ src/metaCompiler/index.ts
@@ -0,0 +1,36 @@
+/// <reference path="../../typings/node/node.d.ts" />
+import {VisitorPass} from "./BNFParser/AbstractManipulators/VisitorPass";
+import {buildVisitor} from './BNFParser/Passes/passIndex';
+import {compile} from './BNFParser/Compiler';
+
+var curInput = "";
+var argv = {};
+var grammar: string =  require('../meta/grammar.json').grammar;
+
+for (var i = 2; i < process.argv.length; i++) {
+    let val = process.argv[i];
+    if (/^-/.test(val)) {
+        curInput = val.substring(1, val.length);
+        argv[curInput] = [];
+    } else {
+        argv[curInput].push(val);
+    }
+}
+
+if (argv.hasOwnProperty('pass')) {
+    doPass(argv['pass'][0]);
+}
+
+export function logArray(arr: string[]) {
+    for (var a of arr) {
+        console.log(a);
+    }
+}
+
+export function doPass(name: string) {
+    var syntaxTree = compile(grammar);
+    let visitor = buildVisitor(name);
+    let visitorPass = new VisitorPass(visitor);
+    visitorPass.visit(syntaxTree);
+    logArray(visitor.output);
+}
