fe4220ce5d373af0eb8388ef1c20cbe72678963e
billytrend <billy.trend@gmail.com>
2015-12-30 12:22:58 +0000

    Fixed pass index.

--- src/metaCompiler/BNFParser/Passes/passIndex.ts
+++ src/metaCompiler/BNFParser/Passes/passIndex.ts
@@ -1,7 +1,7 @@
 import {MakeClassDefinitions} from './MakeClassDefinitions';
 import {MakeDoupleDispatch} from './MakeDoupleDispatch';
 import {MakeLexer} from './MakeLexer';
-import {MakeVisitor} from './MakeVisitor';
+import {MakeVisitorTraversal} from './MakeVisitorTraversal';
 import {ReformatBNF} from './ReformatBNF';
 import {ValidateBNF} from './ValidateBNF';
 import {AbstractVisitor} from "../AbstractManipulators/AbstractVisitor";
@@ -10,17 +10,8 @@ export function buildVisitor(visitorName: string): AbstractVisitor {
     switch (visitorName) {
         case "MakeDoupleDispatch": return new MakeDoupleDispatch();
         case "MakeLexer" : return new MakeLexer();
-        case "MakeVisitor" : return new MakeVisitor();
+        case "MakeVisitorTraversal" : return new MakeVisitorTraversal();
         case "ReformatBNF" : return new ReformatBNF();
         case "ValidateBNF" : return new ValidateBNF();
     }
 }
-
-export var passes: Object = {
-    MakeClassDefinitions: MakeClassDefinitions,
-    MakeDoupleDispatch: MakeDoupleDispatch,
-    MakeLexer: MakeLexer,
-    MakeVisitor: MakeVisitor,
-    ReformatBNF: ReformatBNF,
-    ValidateBNF: ValidateBNF
-};
