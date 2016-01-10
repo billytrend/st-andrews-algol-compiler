cdbdcda0593069b5250f2e51290162fd774e2b19
billytrend <billy.trend@gmail.com>
2016-01-10 12:32:44 +0000

    Improved formatting of BNF by escaping HTML programatically.

--- src/metaCompiler/BNFParser/Passes/ReformatBNF.ts
+++ src/metaCompiler/BNFParser/Passes/ReformatBNF.ts
@@ -4,6 +4,7 @@ import {compile} from "../Compiler";
 import {AbstractVisitor} from "../AbstractManipulators/AbstractVisitor";
 import {VisitorPass} from "../AbstractManipulators/VisitorPass";
 import {Empty} from "../Parser";
+var escape = require('escape-html');
 
 export class ReformatBNF extends AbstractVisitor {
 
@@ -23,12 +24,16 @@ export class ReformatBNF extends AbstractVisitor {
 
     beforeVisitProduction(node:Production) {
         if (this.firstProductionVisit) {
-            this.output.push("<div><h3>" + this.curProductionName + "</h3>");
+            this.output.push("<div><h3>" + escape(this.curProductionName) + "</h3><ul>");
         }
+        this.output.push("<li>")
     }
 
     afterVisitProduction(node:Production) {
-        this.output.push("</div>");
+        this.output.push("</li>");
+        if (this.lastProductionVisit) {
+            this.output.push("</ul></div>");
+        }
     }
 
     beforeVisitTerminal(node:Terminal) {
@@ -40,7 +45,7 @@ export class ReformatBNF extends AbstractVisitor {
     }
 
     beforeVisitNonTerminal(node:NonTerminal) {
-        this.output.push("&lt;" + node.value + "&gt;");
+        this.output.push(escape(node.value));
     }
 
     afterVisitNonTerminal(node:NonTerminal) {
@@ -48,7 +53,7 @@ export class ReformatBNF extends AbstractVisitor {
     }
 
     beforeVisitEmpty(empty:Empty) {
-                this.output.push("E");
+        this.output.push("E");
     }
 
     afterVisitEmpty(empty:Empty) {
